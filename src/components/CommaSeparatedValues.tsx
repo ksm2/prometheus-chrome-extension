7712e8fcdfeb2c41dd03bd2b7ef26e02a380bd3b
Konstantin Möllers <ksm.moellers@gmail.com>
2024-06-06 18:04:04 +0200

    Fix missing key in CSV

--- src/components/CommaSeparatedValues.tsx
+++ src/components/CommaSeparatedValues.tsx
@@ -1,17 +1,17 @@
-import React, { ReactNode } from "react";
+import React, { Fragment, ReactNode } from "react";
 
 export function CommaSeparatedValues({ children }: { children: ReactNode[] }) {
   if (children.length === 0) return null;
   if (children.length === 1) return children[0];
 
   return (
-    <>
+    <Fragment>
       {children.slice(0, -1).map(addComma)}
       {children.at(-1)}
-    </>
+    </Fragment>
   );
 }
 
-function addComma(node: ReactNode): ReactNode {
-  return <>{node}, </>;
+function addComma(node: ReactNode, index: number): ReactNode {
+  return <Fragment key={index}>{node}, </Fragment>;
 }
