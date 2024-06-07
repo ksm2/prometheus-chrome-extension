e2def0a94e08e6b4341c7d977c8033095aa00039
Konstantin Möllers <ksm.moellers@gmail.com>
2024-06-07 20:57:31 +0200

    Fix changing unit propagating

--- src/components/ValueWithUnit.tsx
+++ src/components/ValueWithUnit.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import React, { useState } from "react";
+import React, { useState, MouseEvent } from "react";
 import { format } from "../format";
 import { Unit } from "../model";
 import * as styles from "./styles.module.css";
@@ -13,8 +13,13 @@ export function ValueWithUnit({ unit, children }: Props) {
   const [original, setOriginal] = useState(false);
   const cn = classNames(styles.ValueWithUnit, { [styles.Original]: original });
 
+  function handleClick(e: MouseEvent) {
+    e.stopPropagation();
+    setOriginal((o) => !o);
+  }
+
   return (
-    <span className={cn} onClick={() => setOriginal((o) => !o)}>
+    <span className={cn} onClick={handleClick}>
       {original ? children : format(children, unit)}
     </span>
   );
